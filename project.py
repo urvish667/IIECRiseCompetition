f5469f34b965145710f7cd2f079cdad1174691a9
URVISH TALAVIYA <43312731+urvish667@users.noreply.github.com>
2020-08-23 15:15:40 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
 import os
+import pyttsx3
+import time
 
 def wordFinder(str):
     if "chrome" in str or "browser" in str: 
@@ -18,17 +20,37 @@ def wordFinder(str):
     else:
         return None
 
-while True:
-    print("Enter your requirement: ", end = '')
-    req = input()
-    req.lower()
-    if("run" in req or "show" in req or "execute" in req or "open" in req):
-        if(wordFinder(req) == None):
-            print("Not Understand")
-            continue
-        else:
-            os.system(wordFinder(req))
-    elif("stop" in req or "exit" in req or "exit" in req):
-        break
+def main():
+    t = time.localtime()
+    pyttsx3.speak("Hi!I'm Jane")
+    print("Hi!I'm Jane")
+    if(t.tm_hour < 12):
+        pyttsx3.speak("Good Morning")
+        print("Good Morning")
+    elif(t.tm_hour >= 12 and t.tm_hour <= 16):
+        pyttsx3.speak("Good Aftenoon")
+        print("Good Afternoon")
+    elif(t.tm_hour >= 17 and t.tm_hour <= 8):
+        pyttsx3.speak("Good Evening")
+        print("Good Evening")
     else:
-        print("Not Understand:(")
+        pyttsx3.speak("Good Morning")
+        print("Good Night")
+        
+    while True:
+        print("Enter your requirement: ", end = '')
+        req = input()
+        req.lower()
+        if("run" in req or "show" in req or "execute" in req or "open" in req):
+            if(wordFinder(req) == None):
+                print("Not Understand")
+                continue
+            else:
+                os.system(wordFinder(req))
+        elif("stop" in req or "exit" in req or "exit" in req):
+            break
+        else:
+            print("Not Understand:(")
+            
+if __name__ == "__main__":
+    main()
